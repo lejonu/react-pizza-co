c8003134f501d86083b2a35dfd5ede961282a136
Leonardo José Nunes <leonardo.ferax@gmail.com>
2023-10-30 16:05:04 -0300

    JavaScript Logic in Components

--- src/index.js
+++ src/index.js
@@ -75,10 +75,25 @@ function Menu() {
 }
 
 function Footer() {
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
+
+  // if (hour >= openHour && hour <= closeHour) {
+  //   alert("We're currently open");
+  // } else {
+  //   alert("Sorry we're closed.");
+  // }
+
+  console.log(hour);
+
   return (
     <footer>
-      {new Date().toLocaleTimeString()}. We're currently
-      open
+      {new Date().toLocaleTimeString()}.
+      {isOpen
+        ? "We're currently open "
+        : "Sorry we're closed"}
     </footer>
   );
 
